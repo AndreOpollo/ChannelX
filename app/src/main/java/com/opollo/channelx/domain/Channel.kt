e0ac5d9773d28069f85e2812b09d417d1f69080e
Andre <opolloandres@gmail.com>
2026-02-03 10:19:38 +0300

    configure channel model in the domain

---
+++ app/src/main/java/com/opollo/channelx/domain/Channel.kt
@@ -0,0 +1,19 @@
+package com.opollo.channelx.domain
+
+data class Channel(
+    val id: String,
+    val name: String,
+    val countryCode:String,
+    val streamUrl:String,
+    val imageUrl:String,
+    val isOnline:Boolean,
+    val viewers:Int
+){
+    fun getCountryName():String{
+        return countryCode.uppercase()
+    }
+
+    fun getStatusText():String{
+        return if(isOnline) "Online" else "Offline"
+    }
+}
