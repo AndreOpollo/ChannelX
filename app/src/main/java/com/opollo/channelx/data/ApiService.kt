38af59099f4b9023e9a86051ab8bca4d2ccb13fa
Andre <opolloandres@gmail.com>
2026-02-03 10:12:14 +0300

    [update] API Service

--- app/src/main/java/com/opollo/channelx/data/ApiService.kt
+++ app/src/main/java/com/opollo/channelx/data/ApiService.kt
@@ -1,11 +1,15 @@
 package com.opollo.channelx.data
 
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
 
     @GET("channels")
-    suspend fun getChannels(): ChannelResponseDto
+    suspend fun getChannels(
+        @Query("user") user:String = "cdnlivetv",
+        @Query("plan") plan:String = "free"
+    ): ChannelResponseDto
 
     companion object{
         const val BASE_URL = "https://api.cdn-live.tv/api/v1/"
